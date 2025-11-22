4a201ca1bce6e0861843bcbde7d871a78557d8a7
LavyniaSa <lavyniasa.usa@gmail.com>
2025-11-21 18:27:38 -0600

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number: "))
+
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:" x * y)
+print("Division", x / y)
